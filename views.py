eac3b712228c07b7706992ad7cfbae834f2caabd
sobhan shokri <xd3vman@gmail.com>
2023-02-27 00:04:52 +0330

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
+from django.http import HttpResponse 
 import requests
 import json
+from django.shortcuts import redirect
 
 
 #? sandbox merchant 
@@ -14,33 +16,37 @@ ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest
 ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
 ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
 
-amount = 1000  # Rial / Required
+# amount = 100000
 description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
-phone = 'YOUR_PHONE_NUMBER'  # Optional
-# Important: need to edit for realy server.
-CallbackURL = 'http://127.0.0.1:8080/verify/'
+# phone = '09123456789'  # Optional
+# Important: need to edit for realy server. Add appname
+CallbackURL = 'http://127.0.0.1:8000/appname/verify/'
 
 
 def send_request(request):
+    global amount 
+    amount = request.GET['amount']
     data = {
         "MerchantID": settings.MERCHANT,
         "Amount": amount,
         "Description": description,
-        "Phone": phone,
+        # "Phone": phone,
         "CallbackURL": CallbackURL,
     }
     data = json.dumps(data)
     # set content length by data
-    headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
     try:
         response = requests.post(ZP_API_REQUEST, data=data,headers=headers, timeout=10)
-
         if response.status_code == 200:
             response = response.json()
             if response['Status'] == 100:
-                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']), 'authority': response['Authority']}
+                global authority
+                authority=  response['Authority']
+                return redirect(ZP_API_STARTPAY + str(response['Authority']))
             else:
                 return {'status': False, 'code': str(response['Status'])}
+            
         return response
     
     except requests.exceptions.Timeout:
@@ -49,21 +55,23 @@ def send_request(request):
         return {'status': False, 'code': 'connection error'}
 
 
-def verify(authority):
+def verify(Authority):
+    global Authoritystatus
+    Authoritystatus = Authority.GET['Authority']
     data = {
         "MerchantID": settings.MERCHANT,
         "Amount": amount,
-        "Authority": authority,
+        "Authority": Authoritystatus,
     }
     data = json.dumps(data)
     # set content length by data
-    headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
-    response = requests.post(ZP_API_VERIFY, data=data,headers=headers)
+    headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+    response = requests.post(ZP_API_VERIFY, data=data,headers=headers,timeout=10)
 
     if response.status_code == 200:
         response = response.json()
         if response['Status'] == 100:
-            return {'status': True, 'RefID': response['RefID']}
+            return HttpResponse (str({'status': True, 'RefID': response['RefID']}))
         else:
-            return {'status': False, 'code': str(response['Status'])}
+            return HttpResponse (str({'status': False, 'code': str(response['Status'])}))
     return response
